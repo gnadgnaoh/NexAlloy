0047db6887b17a78a8e2c808c986e411df75050c
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-08-23 05:14:32 +0700

    Add files via upload

--- app/src/main/java/io/github/nexalloy/revanced/zalo/tracking/DisableAdsTrackingPatch.kt
+++ app/src/main/java/io/github/nexalloy/revanced/zalo/tracking/DisableAdsTrackingPatch.kt
@@ -1,17 +1,20 @@
 package io.github.nexalloy.revanced.zalo.tracking
 
 import io.github.nexalloy.patch
-import io.github.nexalloy.revanced.zalo.adtima.zAdsTrackingInventoryFingerprint
+import io.github.nexalloy.revanced.zalo.adtima.blockAdtimaEntryPoints
+import io.github.nexalloy.revanced.zalo.adtima.zAdsTrackingCheckInventoryFingerprint
+import io.github.nexalloy.revanced.zalo.adtima.zAdsTrackingCheckInventoryListFingerprint
 
 val DisableAdsTracking = patch(
     name = "Disable ads tracking",
     description = "Blocks Adtima impression/click reporting and the Zalo ads tracking receiver.",
 ) {
-    ::zAdsTrackingInventoryFingerprint.hookMethod {
-        before { param -> param.result = null }
-    }
-
-    ::adsTrackingReceiverFingerprint.hookMethod {
-        before { param -> param.result = null }
-    }
+    blockAdtimaEntryPoints(
+        "Zalo ads tracking",
+        listOf(
+            "inventoryCheck" to ::zAdsTrackingCheckInventoryFingerprint,
+            "inventoryCheckBatch" to ::zAdsTrackingCheckInventoryListFingerprint,
+            "trackingReceiver" to ::adsTrackingReceiverFingerprint,
+        ),
+    )
 }
